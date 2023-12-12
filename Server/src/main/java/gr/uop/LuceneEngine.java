90c7c286bcc487c30d12477233e6fe063556fe12
Vasilis Koulouris <basileskouloures@gmail.com>
2023-12-12 15:05:40 +0200

    changed implementation to create modified data

--- Server/src/main/java/gr/uop/LuceneEngine.java
+++ Server/src/main/java/gr/uop/LuceneEngine.java
@@ -1,9 +1,14 @@
 package gr.uop;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,6 +17,10 @@ import java.util.Comparator;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.CSVFormat.Builder;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
@@ -25,6 +34,7 @@ public class LuceneEngine {
     public static final String songsFilePath  = modifiedDataDir+"/songsM.csv";
     public static final String albumsFilePath = modifiedDataDir+"/albumsM.csv";
     public static final String lyricsFilePath = modifiedDataDir+"/lyricsM.csv";
+    private static final String websiteLink = "https://www.azlyrics.com";
     private Indexer indexer;
     private Searcher searcher;
     private final int MAX_RESULTS;
@@ -46,79 +56,121 @@ public class LuceneEngine {
     }
 
     private void createModifiedData() throws IOException {
-        PrintWriter pr;
-        Scanner scan;
         Path modifiedDataPath = Paths.get(modifiedDataDir);
         File dir = new File(modifiedDataDir);
         if(dir.isDirectory() && dir.exists()){System.out.println("preprocessesed data exists");return;}
         Files.createDirectories(modifiedDataPath);
         File[] files = new File(rawdataDir).listFiles();
         for (File file : files) {
-            if(!file.isDirectory() && !file.isHidden() && file.exists() && file.canRead() && filter.accept(file)){
+            if(!file.isDirectory() && !file.isHidden() && file.exists() && file.canRead() && filter.accept(file))
+            {
+                final String iNameField = "i";//first column, added manually
+                final String singerNameField = "singer_name";
+                final String songNameField = "song_name";
+                final String songHrefField = "song_href";
+                Reader in = new FileReader(file);
+
                 if(file.getName().equalsIgnoreCase("songs.csv")){//preprocessing for songs
-                    File songs = new File(songsFilePath);
-                    songs.createNewFile();
-                    pr = new PrintWriter(songs);
-                    scan = new Scanner(file);
-                    int count = 0;
-                    while(scan.hasNext()){
-                        String line = scan.nextLine();
-                        StringTokenizer tok = new StringTokenizer(line, ",");
-                        if(count != 0){
-                            tok.nextToken();
-                        }
-                        tok.nextToken();
-                        String singerName = tok.nextToken();
-                        String songName = tok.nextToken();
-                        String songHref = tok.nextToken();
-                        if(!singerName.equalsIgnoreCase("singer_name")){
-                            singerName = singerName.substring(0, singerName.indexOf(" Lyrics"));
-                        }
-                        if(!songHref.equalsIgnoreCase("song_href")){
-                            songHref = songHref.substring(2);
-                            songHref = "https://www.azlyrics.com"+songHref;
-                        }
-                        pr.println(singerName+","+songName+","+songHref);
-                        count += 1;
-                    }
+                    File modifiedCSV = new File(songsFilePath);
+                    modifiedCSV.createNewFile();
+                    BufferedWriter out = new BufferedWriter(new FileWriter(modifiedCSV));
+                    final String[] inputHeaders = {iNameField,"song_id",singerNameField,songNameField,songHrefField};
+                    final String[] outputHeaders = {singerNameField,songNameField, songHrefField};
+
+                    Builder csvInFileBuilder = CSVFormat.DEFAULT.builder();
+                    csvInFileBuilder.setHeader(inputHeaders);
+                    csvInFileBuilder.setSkipHeaderRecord(true);
+                    CSVFormat songCsvFormat = csvInFileBuilder.build();
+
+                    Builder csvOutFileBuilder = CSVFormat.DEFAULT.builder();
+                    csvOutFileBuilder.setHeader(outputHeaders);
+                    CSVFormat outCsvFormat = csvOutFileBuilder.build();
+                    CSVPrinter csvPr = new CSVPrinter(out, outCsvFormat);
+
+                    Iterable<CSVRecord> records = songCsvFormat.parse(in);
+                    for(CSVRecord r: records){
+                        String singerName = r.get(singerNameField);
+                        String songName = r.get(songNameField);
+                        String songHref = r.get(songHrefField);
+                        singerName = configureArtistName(singerNameField, singerName);
+                        songHref = configureLink(songHrefField, songHref);
+                        csvPr.printRecord(singerName, songName, songHref);
+                    }csvPr.close();
 
                 }else if(file.getName().equalsIgnoreCase("albums.csv")){//preprocessing for albums
-                    File albums = new File(albumsFilePath);
-                    albums.createNewFile();
-                    pr = new PrintWriter(albums);
-                    scan = new Scanner(file);
-                    int count = 0;
-                    while(scan.hasNext()){
-                        String line = scan.nextLine();
-                        StringTokenizer tok = new StringTokenizer(line, ",");
-                        if(count != 0){
-                            tok.nextToken();
-                        }
-                        String mightBeSingerName = tok.nextToken();//id in second column might be missing
-                        String singerName;
-                        try{
-                            Integer.parseInt(mightBeSingerName);
-                            singerName = tok.nextToken();
-                        }catch(NumberFormatException e){
-                            if(mightBeSingerName.equalsIgnoreCase("id")){
-                                singerName = tok.nextToken();
-                            }else{singerName = mightBeSingerName;}
-                        } 
-                        String albumName = tok.nextToken();
-                        String albumType = tok.nextToken();
-                        String albumYear = tok.nextToken();
-                        if(!singerName.equalsIgnoreCase("singer_name")){
-                            singerName = singerName.substring(0, singerName.indexOf(" Lyrics"));
-                        }
-                        pr.println(singerName+","+albumName+","+albumType+","+albumYear);
-                        count += 1;
-                    }
+                    File modifiedCSV = new File(albumsFilePath);
+                    modifiedCSV.createNewFile();
+                    BufferedWriter out = new BufferedWriter(new FileWriter(modifiedCSV));
+                    final String nameField = "name", typeField="type", yearField = "year";
+                    final String[] inputHeaders = {iNameField,"id",singerNameField, nameField, typeField, yearField};
+                    final String[] outputHeaders = {singerNameField, nameField, typeField, yearField};
+
+                    Builder csvInFileBuilder = CSVFormat.DEFAULT.builder();
+                    csvInFileBuilder.setHeader(inputHeaders);
+                    csvInFileBuilder.setSkipHeaderRecord(true);
+                    CSVFormat albumCsvFormat = csvInFileBuilder.build();
+
+                    Builder csvOutFileBuilder = CSVFormat.DEFAULT.builder();
+                    csvOutFileBuilder.setHeader(outputHeaders);
+                    CSVFormat outCsvFormat = csvOutFileBuilder.build();
+                    CSVPrinter csvPr = new CSVPrinter(out, outCsvFormat);
+
+                    Iterable<CSVRecord> records = albumCsvFormat.parse(in);
+                    for(CSVRecord r: records){
+                        String singerName = r.get(singerNameField);
+                        String albumName = r.get(nameField);
+                        String albumType = r.get(typeField);
+                        String albumYear = r.get(yearField);
+                        singerName = configureArtistName(singerNameField, singerName);
+                        csvPr.printRecord(singerName, albumName, albumType, albumYear);
+                    }csvPr.close();
 
                 }else{//preprocessing for lyrics
+                    File modifiedCSV = new File(lyricsFilePath);
+                    modifiedCSV.createNewFile();
+                    BufferedWriter out = new BufferedWriter(new FileWriter(modifiedCSV));
+                    final String songLyricsField = "lyrics";
+                    final String[] inputHeaders = {iNameField,songHrefField,singerNameField,songNameField,songLyricsField};
+                    final String[] outputHeaders = {songHrefField,singerNameField,songNameField,songLyricsField};
 
+                    Builder csvInFileBuilder = CSVFormat.DEFAULT.builder();
+                    csvInFileBuilder.setHeader(inputHeaders);
+                    csvInFileBuilder.setSkipHeaderRecord(true);
+                    CSVFormat lyricsCsvFormat = csvInFileBuilder.build();
+
+                    Builder csvOutFileBuilder = CSVFormat.DEFAULT.builder();
+                    csvOutFileBuilder.setHeader(outputHeaders);
+                    CSVFormat outCsvFormat = csvOutFileBuilder.build();
+                    CSVPrinter csvPr = new CSVPrinter(out, outCsvFormat);
+
+                    Iterable<CSVRecord> records = lyricsCsvFormat.parse(in);
+                    for(CSVRecord r: records){
+                        String songHref = r.get(songHrefField);
+                        String singerName = r.get(singerNameField);
+                        String songName = r.get(songNameField);
+                        String lyrics = r.get(songLyricsField);
+                        singerName = configureArtistName(singerNameField, singerName);
+                        songHref = configureLink(songHrefField, songHref);
+                        csvPr.printRecord(songHref, singerName, songName, lyrics);
+                    }csvPr.close();
                 }
             }
         }
+    }
+
+    private String configureArtistName(String singerNameField, String artist) {
+        if(!artist.equalsIgnoreCase(singerNameField)){
+            artist = artist.substring(0, artist.indexOf(" Lyrics"));
+        }
+        return artist;
+    }
+
+    private String configureLink(String linkFieldName, String link) {
+        if(!link.equalsIgnoreCase(linkFieldName)){
+            link = link.substring(2);
+            link = websiteLink+link;
+        }
+        return link;
     }
 
     public void search(String searchQuery) throws IOException, ParseException {
